578aec4e811eb23b30f536b737250c34669d1cf0
overnightover <yunbanana@qq.com>
2023-08-07 18:15:56 +0800

    优化f04.py代码并添加.gitignore规则
    - 优化了f04.py的代码，使用pandas的read_sql_query函数直接读取到DataFrame，简化了代码结构。
    - 在处理数据表名称时，添加了跳过空白行的逻辑。
    - 将生成的Excel文件添加到.gitignore文件中，避免将这些文件提交到版本控制系统。
    - 创建了一个新的文件f04_chatgptAjust.py，这是对f04.py的优化版本，使用了更简洁的路径构造方式和字符串格式化方式。

--- py/f04.py
+++ py/f04.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook
+import pandas as pd
 import cx_Oracle
 import os
 
@@ -15,33 +15,28 @@ with open(cur_path+"\csv\数据表名称.txt","r") as f:
     for line in f.readlines():
         try:
             table = line.strip()
+            # 跳过空白行
+            if not table:
+                continue
             print(table)
             sql = "select * from %s"%(table)
             cur.execute(sql)          # 执行sql查询
         except Exception as e:
             print("导出失败数据表为%s，失败原因为"%(table),e)
             continue
-        results = cur.fetchall()     # 获取所有查询结果
 
-        # 获取行和列
-        rows = len(results)
-        if len(results):
-            cols = len(results[0])
+        # 获取查询结果
+        rows = cur.fetchall()
 
-        # 创建表格
-        wb = Workbook()
-        ws = wb.create_sheet("%s"%(table),0)
+        # 获取列名
+        columns = [i[0] for i in cur.description]
 
-        # 获取表头的字段值，即标题行
-        db_title = [i[0] for i in cur.description]
-        for i,description in enumerate(db_title):
-            ws.cell(row=1, colum=1+i).value=description
+        # 创建pandas DataFrame
+        df = pd.DataFrame(rows, columns=columns)
 
-        # 循环查询结果行和列，存在excel中
-        for m in range(rows):
-            for n in range(cols):
-                ws.cell(row=m+2,colum=n+1).value=results[2][n]
-        wb.save(cur_path+"\csv\{}.xlsx".format(table))
+        # 导出为Excel文件
+        df.to_excel(cur_path+"\csv\{}.xlsx".format(table), index=False)
+
 
 # 关闭游标和链接
 cur.close()
